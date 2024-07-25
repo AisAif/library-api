d275d9a1fd4997b08d55f0d2965fd7e632c7bfb0
AisAif <Isbahrulfahni@gmail.com>
2024-07-25 13:59:21 +0700

    feat: import auth and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '@/database/database.module';
 import appConfig from '@config/app.config';
 import databaseConfig from '@config/database.config';
+import { AuthModule } from '@/auth/auth.module';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import databaseConfig from '@config/database.config';
       load: [appConfig, databaseConfig],
     }),
     DatabaseModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
