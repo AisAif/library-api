718a71bcddfb440d597ca574e09b23a25a27e21f
AisAif <Isbahrulfahni@gmail.com>
2024-07-25 13:56:13 +0700

    feat: add user register dto

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password_confirm: string;
+}
