055af87a3a7a89eb97cc9c701868a40a6bfac69b
AisAif <Isbahrulfahni@gmail.com>
2024-07-25 14:05:18 +0700

    test(auth): setup auth e2e test and make register e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,107 @@
+import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { TestAppModule } from './test.app.module';
+import { DataSource } from 'typeorm';
+import { AuthModule } from '@/auth/auth.module';
+
+describe('Auth', () => {
+  let app: INestApplication;
+  let dataSource: DataSource;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TestAppModule, AuthModule],
+    }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+    dataSource = app.get(DataSource);
+  });
+
+  afterEach(async () => {
+    return Promise.all([dataSource.query('DELETE FROM users')]);
+  });
+
+  afterAll(async () => {
+    return Promise.all([dataSource.destroy(), app.close()]);
+  });
+
+  it('POST /auth/register | should return 201', async () => {
+    const result = await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        name: 'test',
+        username: 'test',
+        password: 'testtest',
+        password_confirm: 'testtest',
+      });
+
+    expect(result.status).toBe(201);
+    return;
+  });
+
+  it('POST /auth/register | should return 400 when field is blank', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send()
+      .expect(400)
+      .expect({
+        message: [
+          'username must be longer than or equal to 3 characters',
+          'username should not be empty',
+          'username must be a string',
+          'name must be longer than or equal to 3 characters',
+          'name should not be empty',
+          'name must be a string',
+          'password must be longer than or equal to 8 characters',
+          'password should not be empty',
+          'password must be a string',
+          'password_confirm must be longer than or equal to 8 characters',
+          'password_confirm should not be empty',
+          'password_confirm must be a string',
+        ],
+        error: 'Bad Request',
+        statusCode: 400,
+      });
+  });
+
+  it('POST /auth/register | should return 400 when password not match', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        name: 'test',
+        username: 'test',
+        password: 'testtests',
+        password_confirm: 'testtest',
+      })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: 'password not match',
+        error: 'Bad Request',
+      });
+  });
+
+  it('POST /auth/register | should return 400 when username already exists', async () => {
+    await dataSource.query(
+      'INSERT INTO users (name, username, password) VALUES (?, ?, ?)',
+      ['test', 'test', 'testtest'],
+    );
+
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        name: 'test',
+        username: 'test',
+        password: 'testtests',
+        password_confirm: 'testtest',
+      })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: 'password not match',
+        error: 'Bad Request',
+      });
+  });
+});
