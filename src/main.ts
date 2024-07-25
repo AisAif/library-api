3c350b7786304635275351ecbed9c6a2ac75f367
AisAif <Isbahrulfahni@gmail.com>
2024-07-25 13:37:31 +0700

    chore: add config service and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Libary API')
-    .setDescription('Libary API is a REST API for library management system')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-doc', app, document);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('app.port') || 3000);
 }
 bootstrap();
