b9b6f8257ff6908cbfa551e3d691508ba1fc2d7e
AisAif <Isbahrulfahni@gmail.com>
2024-07-25 13:31:58 +0700

    style: add app and database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from '@config/app.config';
+import databaseConfig from '@config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
