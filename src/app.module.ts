f69b0e61c689f56256aae9d0abff362b04bd1ea2
AisAif <Isbahrulfahni@gmail.com>
2024-07-27 00:13:12 +0700

    chore: register jwt auth guard and jwt strategy providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import databaseConfig from '@config/database.config';
 import { AuthModule } from '@/auth/auth.module';
 import { UsersModule } from '@/users/users.module';
 import jwtConfig from '@config/jwt.config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { JwtStrategy } from '@/auth/strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -18,6 +21,12 @@ import jwtConfig from '@config/jwt.config';
     UsersModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    JwtStrategy,
+  ],
 })
 export class AppModule {}
