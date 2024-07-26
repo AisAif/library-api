d202971f142d3437c52cd5b8e82f0d69e171315b
AisAif <Isbahrulfahni@gmail.com>
2024-07-27 00:09:18 +0700

    feat(auth): add jwt strategy for get user current logged in from req

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserData } from '@/users/users.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwt.secret'),
+    });
+  }
+
+  async validate(payload: UserData): Promise<UserData> {
+    return {
+      username: payload.username,
+      name: payload.name,
+      role: payload.role,
+    };
+  }
+}
