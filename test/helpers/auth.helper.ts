ee293294b5bb45f7db18d33bfee33a33080c9a06
AisAif <Isbahrulfahni@gmail.com>
2024-07-28 21:34:58 +0700

    test(helpers): add parameter in helper function

--- test/helpers/auth.helper.ts
+++ test/helpers/auth.helper.ts
@@ -2,11 +2,13 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 
 export const getAccessToken = async (
+  username: string,
+  password: string,
   app: INestApplication,
 ): Promise<string> => {
   const response = await request(app.getHttpServer())
     .post('/auth/login')
-    .send({ username: 'test', password: 'testtest' })
+    .send({ username, password })
     .expect(200);
 
   return response.body.data.access_token;
--- test/helpers/user.helper.ts
+++ test/helpers/user.helper.ts
@@ -1,12 +1,14 @@
-import { User, UserRole } from '@/users/users.entity';
+import { User } from '@/users/users.entity';
 import { DataSource } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
-export const createTestUser = async (dataSource: DataSource): Promise<User> => {
+export const createTestUser = async (
+  user: User,
+  dataSource: DataSource,
+): Promise<User> => {
   return await dataSource.getRepository(User).save({
-    name: 'test',
-    username: 'test',
-    password: await bcrypt.hash('testtest', 10),
-    role: UserRole.USER,
+    name: user.name,
+    username: user.username,
+    password: await bcrypt.hash(user.password, 10),
   });
 };
