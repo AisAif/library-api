a8b8fd87002a1ab873759065a874b74c4d416b38
AisAif <Isbahrulfahni@gmail.com>
2024-07-28 22:01:06 +0700

    feat(users): add update user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
+import { UpdateUserDTO } from '@/auth/dto/update-user.dto';
+import { UserData } from './users.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +15,55 @@ export class UsersService {
   }
   async findOne(username: string): Promise<User | null> {
     return this.usersRepository.findOneBy({ username });
+  }
+
+  async update(
+    username: string,
+    updateUserDTO: UpdateUserDTO,
+  ): Promise<UserData> {
+    if (
+      updateUserDTO.password &&
+      updateUserDTO.password !== updateUserDTO.password_confirm
+    ) {
+      throw new BadRequestException(['password not match']);
+    }
+
+    if (await this.findOne(updateUserDTO.username)) {
+      throw new BadRequestException(['username already exists']);
+    }
+
+    const user = await this.findOne(username);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password_confirm: _, ...updateUserData } = updateUserDTO;
+
+    if (updateUserData.password) {
+      updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+    }
+
+    await this.usersRepository.update(
+      { username },
+      {
+        ...user,
+        ...updateUserData,
+      },
+    );
+
+    let result: User;
+
+    if (updateUserDTO.username) {
+      result = await this.findOne(updateUserDTO.username);
+    } else {
+      result = await this.findOne(username);
+    }
+
+    return {
+      name: result.name,
+      username: result.username,
+    };
   }
 }
