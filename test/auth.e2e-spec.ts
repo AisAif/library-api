104f44c633e97cf3e000abd9d5c8dd2bcc52328a
AisAif <Isbahrulfahni@gmail.com>
2024-07-28 22:07:46 +0700

    test(auth): make update profile e2e test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -7,6 +7,8 @@ import { AuthModule } from '@/auth/auth.module';
 import { createTestUser } from './helpers/user.helper';
 import { getAccessToken } from './helpers/auth.helper';
 import { ResponseInterceptor } from '@/common/interceptors/response.interceptor';
+import { User } from '@/users/users.entity';
+import * as bcrypt from 'bcrypt';
 
 describe('Auth', () => {
   let app: INestApplication;
@@ -83,7 +85,7 @@ describe('Auth', () => {
         .expect(400)
         .expect({
           statusCode: 400,
-          message: 'password not match',
+          message: ['password not match'],
           error: 'Bad Request',
         });
     });
@@ -99,13 +101,13 @@ describe('Auth', () => {
         .send({
           name: 'test',
           username: 'test',
-          password: 'testtests',
+          password: 'testtest',
           password_confirm: 'testtest',
         })
         .expect(400)
         .expect({
           statusCode: 400,
-          message: 'password not match',
+          message: ['username already exists'],
           error: 'Bad Request',
         });
     });
@@ -113,7 +115,14 @@ describe('Auth', () => {
 
   describe('POST /auth/login', () => {
     beforeEach(async () => {
-      await createTestUser(dataSource);
+      await createTestUser(
+        {
+          name: 'test',
+          username: 'test',
+          password: 'testtest',
+        },
+        dataSource,
+      );
     });
 
     it('should return 200 and access token', async () => {
@@ -137,13 +146,20 @@ describe('Auth', () => {
     });
   });
 
-  describe('POST /auth/profile', () => {
+  describe('GET /auth/profile', () => {
     beforeEach(async () => {
-      await createTestUser(dataSource);
+      await createTestUser(
+        {
+          name: 'test',
+          username: 'test',
+          password: 'testtest',
+        },
+        dataSource,
+      );
     });
 
     it('should return 200', async () => {
-      const accessToken = await getAccessToken(app);
+      const accessToken = await getAccessToken('test', 'testtest', app);
       const result = await request(app.getHttpServer())
         .get('/auth/profile')
         .set('Authorization', `Bearer ${accessToken}`);
@@ -151,12 +167,91 @@ describe('Auth', () => {
       expect(result.status).toBe(200);
       expect(result.body.data).toHaveProperty('username');
       expect(result.body.data).toHaveProperty('name');
-      expect(result.body.data).toHaveProperty('role');
       return;
     });
 
     it('should return 401 when access token not provided or invalid', async () => {
       return request(app.getHttpServer()).get('/auth/profile').expect(401);
+    });
+  });
+
+  describe('PATCH /auth/profile', () => {
+    beforeEach(async () => {
+      await createTestUser(
+        {
+          name: 'test',
+          username: 'test',
+          password: 'testtest',
+        },
+        dataSource,
+      );
+    });
+
+    it('should return 200', async () => {
+      const accessToken = await getAccessToken('test', 'testtest', app);
+      const result = await request(app.getHttpServer())
+        .patch('/auth/profile')
+        .send({
+          name: 'test1',
+          username: 'test1',
+          password: 'testtest1',
+          password_confirm: 'testtest1',
+        })
+        .set('Authorization', `Bearer ${accessToken}`);
+
+      expect(result.status).toBe(200);
+      const user = await dataSource.getRepository(User).findOneBy({
+        username: 'test1',
+      });
+      expect(user.username).toBe('test1');
+      expect(user.name).toBe('test1');
+      expect(await bcrypt.compare('testtest1', user.password)).toBe(true);
+      return;
+    });
+
+    it('should return 400 when password not empty but password_confirm empty', async () => {
+      const accessToken = await getAccessToken('test', 'testtest', app);
+      return request(app.getHttpServer())
+        .patch('/auth/profile')
+        .send({
+          password: 'testtest1',
+        })
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(400)
+        .expect({
+          message: [
+            'password_confirm should not be empty',
+            'password_confirm must be longer than or equal to 8 characters',
+            'password_confirm must be a string',
+          ],
+          error: 'Bad Request',
+          statusCode: 400,
+        });
+    });
+
+    it('should return 400 when username already exist', async () => {
+      await createTestUser(
+        {
+          name: 'test1',
+          username: 'test1',
+          password: 'testtest1',
+        },
+        dataSource,
+      );
+
+      const accessToken = await getAccessToken('test', 'testtest', app);
+      return request(app.getHttpServer())
+        .patch('/auth/profile')
+        .send({
+          username: 'test1',
+        })
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(400)
+        .expect({
+          message: ['username already exists'],
+          error: 'Bad Request',
+          statusCode: 400,
+        });
     });
   });
 });
