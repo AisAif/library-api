340d1f8e4054dd7f39131cd94db67c0a8402a314
AisAif <Isbahrulfahni@gmail.com>
2024-07-29 21:13:21 +0700

    feat(books): add find all books service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -3,6 +3,7 @@ import { Book } from './books.entity';
 import { Repository } from 'typeorm';
 import { UsersService } from '@/users/users.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { paginate, Paginated, PaginateQuery } from 'nestjs-paginate';
 
 @Injectable()
 export class BooksService {
@@ -26,5 +27,44 @@ export class BooksService {
 
     user.books = [book];
     await this.usersService.save(user);
+  }
+  async findAll(
+    username: string,
+    query: PaginateQuery,
+  ): Promise<Paginated<Book>> {
+    const user = await this.usersService.findOne(username);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const bookQuery = this.booksRepository
+      .createQueryBuilder('book')
+      .where('userUsername = :username', { username });
+
+    return paginate(query, bookQuery, {
+      sortableColumns: [
+        'id',
+        'title',
+        'author',
+        'summary',
+        'total_page',
+        'status',
+        'created_at',
+        'updated_at',
+      ],
+      nullSort: 'last',
+      defaultSortBy: [['id', 'DESC']],
+      searchableColumns: ['title', 'author', 'summary'],
+      select: [
+        'id',
+        'title',
+        'author',
+        'summary',
+        'total_page',
+        'status',
+        'created_at',
+        'updated_at',
+      ],
+    });
   }
 }
